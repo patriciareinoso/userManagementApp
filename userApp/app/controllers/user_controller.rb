7e56c813e65f5a12e48a3a566bc603d2efbd4002
Patricia Reinoso <patricia.reinoso12@gmail.com>
2017-05-27 23:59:33 +0200

    Basic format validations on added

--- userApp/app/controllers/user_controller.rb
+++ userApp/app/controllers/user_controller.rb
@@ -1,17 +1,29 @@
+# Controller for the user class
+#
+# Author: Patricia Reinoso
+
+# This class include all the methods for CRUD operations
+# of an user
 class UserController < ApplicationController
 
+	# List all the users
 	def index
 		@user = User.all
 	end
 
+	# Class constructor
 	def new 
 		@user = User.new
 	end
 	
+
+	# Find an user according to the id
 	def edit
 		@user = User.find(params[:id])
 	end
 
+	# Create a new user. If the user creation
+	# is valid. Redirect to user list.
 	def create
 		@user = User.new(user_params)
 		
@@ -22,6 +34,8 @@ class UserController < ApplicationController
 		end
 	end
 
+	# Update an user. If the user update
+	# is valid. Redirect to show user
 	def update
 		@user = User.find(params[:id])
 
@@ -32,10 +46,12 @@ class UserController < ApplicationController
 		end
 	end
 
+	# Show the user details.
 	def show
 		@user = User.find(params[:id])
 	end
 
+	# Deletes an user and redirect the the user list.
 	def destroy
 		@user = User.find(params[:id])
 		@user.destroy
@@ -43,6 +59,7 @@ class UserController < ApplicationController
 		redirect_to user_index_path
 	end
 
+	# Validate params on the form.
 	private
 		def user_params	
 			params.require(:user).permit(:family_name, :name, :email, :birthday, :phone, :cellphone, :bio, :carreer, :rate)
--- userApp/app/models/user.rb
+++ userApp/app/models/user.rb
@@ -1,13 +1,44 @@
+# Includes the validations of attributes on the
+# classes
+# 
+# Author:: Patricia Reinoso
+
+
+# Class for attribute validation on an User.
+# * family name and name only allow letters and spaces and
+# * their max length is 50
+# * email format is validated (eg. email@domain.xx )
+# * birthday can not be in the future
+# * bio and carrer allow maximum 300 characters.
+# * rate must be non negative
+# * family_name, name, emails, birthday, cellphone, rate cannot be null.
+# * Formats allowed for phone and cellphone are:
+# * 0612345678, 06 12 34 56 78, +33 6 12 34 56 78, +33612345678
+# * Only aread code +33 is valid.
 class User < ActiveRecord::Base
-	validates :family_name, presence: true
-	validates :name, presence: true
-	validates :email, presence: true
+
+	validates :family_name, presence: true, 
+							format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" },
+							length: { maximum: 50, too_long: "%{count} characters is the maximum allowed"}
+	validates :name, presence: true, 
+					 format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" },
+					 length: { maximum: 50, too_long: "%{count} characters is the maximum allowed"}
+	validates :email, presence: true,
+					  format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "invalid format" }
 	validates :birthday, presence: true
-	validates :phone, presence: true
-	validates :cellphone, presence: true
-	validates :bio, presence: true
-	validates :carreer, presence: true
-	validates :rate, presence: true, length: {minimum: 0}
+	validate :birthday_cannot_be_in_the_future
+	validates :phone, format: { with: /\A^0\d(?:\d{8}|(?: \d\d){4})|\+33 0\d(?: \d\d){4}|\+33(?:\d{9})$\z/}
+	validates :cellphone, presence: true,
+						  format: { with: /\A^0\d(?:\d{8}|(?: \d\d){4})|\+33 0\d(?: \d\d){4}|\+33(?:\d{9})$\z/}
+	validates :bio, length: { maximum: 300, too_long: "%{count} characters is the maximum allowed" }
+	validates :carreer, length: { maximum: 300, too_long: "%{count} characters is the maximum allowed" }
+	validates :rate, presence: true, numericality:{ :greater_than_or_equal_to => 0}
 
 	
+
+	def birthday_cannot_be_in_the_future
+		if birthday > Date.today
+			errors.add(:birthday, "invalid")
+		end
+	end
 end
